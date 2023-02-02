ef479493e02c8fe0a85a24772e1ee7dca2faa010
j-mascarenhas-tc <josemar.mascarenhas@tc.com.br>
2023-02-02 16:46:29 -0300

    depends on :construction_worker:

--- projeto-integrador/cloudflare.tf
+++ projeto-integrador/cloudflare.tf
@@ -6,11 +6,12 @@ variable "domain" {
   default = "turma3-elevenworks.com"
 }
 resource "cloudflare_record" "www" {
-  domain  = var.domain
-  name    = "www"
-  value   = aws_eip.eip.public_ip
-  type    = "A"
-  proxied = true
+  domain     = var.domain
+  name       = "www"
+  value      = aws_eip.eip.public_ip
+  type       = "A"
+  proxied    = true
+  depends_on = [aws_eip.eip]
 }
 resource "cloudflare_zone_settings_override" "turma3-elevenworks.-settings" {
   name = var.domain
