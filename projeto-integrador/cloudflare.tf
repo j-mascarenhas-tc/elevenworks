5a8f54083752ce00dbe9c2832edb67dd91d1c5cd
j-mascarenhas-tc <josemar.mascarenhas@tc.com.br>
2023-02-02 16:34:25 -0300

    add cloudflare :construction_worker:

---
+++ projeto-integrador/cloudflare.tf
@@ -0,0 +1,23 @@
+provider "cloudflare" {
+  email = "you@example.com"
+  token = "your-api-key"
+}
+variable "domain" {
+  default = "turma3-elevenworks.com"
+}
+resource "cloudflare_record" "www" {
+  domain  = var.domain
+  name    = "www"
+  value   = aws_eip.eip.public_ip
+  type    = "A"
+  proxied = true
+}
+resource "cloudflare_zone_settings_override" "turma3-elevenworks.-settings" {
+  name = var.domain
+  settings {
+    tls_1_3                  = "on"
+    automatic_https_rewrites = "on"
+    ssl                      = "strict"
+    waf                      = "on"
+  }
+}
